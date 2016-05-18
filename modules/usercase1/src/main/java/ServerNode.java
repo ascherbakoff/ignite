72246c4daed9bee74740608d519047d3d4dd85cd
Aleksei Scherbakov <alexey.scherbakoff@gmail.com>
2016-05-18 19:25:50 +0300

    Added to the suite.

--- modules/usercase1/src/main/java/ServerNode.java
+++ modules/usercase1/src/main/java/ServerNode.java
@@ -15,31 +15,36 @@
  * limitations under the License.
  */
 
+import java.util.List;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.ignite.cache.query.QueryCursor;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
 
 /**
  *
  */
 public class ServerNode {
-
-    private static Logger log = LoggerFactory.getLogger(ServerNode.class);
-    private static final String CACHE_NAME = "query";
-
     public static void main(String[] args) throws InterruptedException {
-        /*try (*/Ignite ignite = Ignition.start("example-ignite.xml");/*)*/ {
-            log.info("Server Start with id {}!!!", ignite.cluster().localNode().id());
+        try(Ignite ignite = Ignition.start("example-ignite.xml")) {
+            IgniteCache<ActivityKey, Activity> activity = ignite.cache("activity");
 
-            Thread.sleep(3000);
-
-//            ignite.close();
-
-
-        } /*finally {
-            log.info("Server Stop!!!");
-        }*/
+            SqlFieldsQuery qry = new SqlFieldsQuery("explain SELECT activity0.* FROM activity activity0\n" +
+                "LEFT OUTER JOIN \"activityhistory\".activityhistory activityhistory0 ON activityhistory0.activityhistoryId = activity0.lastactivityId\n" +
+                "LEFT OUTER JOIN \"activityuseraccountrole\".activityuseraccountrole activityuseraccountrole0 ON activityuseraccountrole0.activityId = activity0.activityId\n" +
+                "LEFT OUTER JOIN \"activityhistoryuseraccount\".activityhistoryuseraccount activityhistoryuseraccount0 ON activityhistoryuseraccount0.activityHistoryId = activityhistory0.activityhistoryId\n" +
+                "WHERE activity0.kernelId IS NULL\n" +
+                "AND activity0.realizationId IS NULL\n" +
+                "AND NOT activityhistory0.activitystateEnumid IN (37, 30, 463, 33, 464)\n" +
+                "AND (\n" +
+                "(activityuseraccountrole0.useraccountroleId IN (1, 3) AND (activity0.removefromworklist = 0 OR activityhistoryuseraccount0.userAccountId IS NULL))\n" +
+                "OR activityhistoryuseraccount0.useraccountId = 600301\n" +
+                ")");
+            qry.setLocal(true);
+            QueryCursor<List<?>> query = activity.query(qry);
+            System.out.println(query.getAll());
+        };
     }
 
 }
